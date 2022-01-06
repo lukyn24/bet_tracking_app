3ada66711e2f05714a2a93cd8176b16a3597f154
Lukas Hejduk <lukas.hejduk@brighten.digital>
2022-01-06 23:24:22 +0100

    xx

--- data/index.js
+++ data/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userData = require('./users_data.js');
 const User = require('../models/user');
 
-mongoose.connect((process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || /* 'mongodb+srv://lukash:10295monika@cluster0.kd5x9.mongodb.net' || */ 'mongodb://127.0.0.1:27017/sazkyApp'), { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect((process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 
+'mongodb+srv://lukash:10295monika@cluster0.kd5x9.mongodb.net' || 'mongodb://127.0.0.1:27017/sazkyApp') , { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('CONNECION OPEN!!');
 })
